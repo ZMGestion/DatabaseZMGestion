09badfc728f881dd0f9d4297516e13fb1547bec9
Loik <loikchoua4@gmail.com>
2020-04-09 15:24:28 -0300

    JSON como salida en procedimientos

--- StoredProcedures/Roles/zsp_rol_listar_permisos.sql
+++ StoredProcedures/Roles/zsp_rol_listar_permisos.sql
@@ -4,10 +4,26 @@ CREATE PROCEDURE `zsp_rol_listar_permisos`(pIdRol tinyint)
 
 BEGIN
 	/*
-		Lista todos los permisos existentes para un rol
+		Lista todos los permisos existentes para un rol y devuelve un JSON en pSalida.
 	*/
+
+    GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
+    SELECT JSON_OBJECT('Error', CONCAT("ERROR ", COALESCE(@errno, ''), " (", COALESCE(@sqlstate, ''), "): ", COALESCE(@text, ''))) pSalida;
     
-    SELECT	* 
+    SELECT	
+    CONCAT(
+        '[',
+        COALESCE(
+            GROUP_CONCAT(
+                JSON_OBJECT(
+                    'IdPermiso', IdPermiso,
+                    'Permiso', Permiso,
+                    'Procedimiento', Procedimiento,
+                    'Descripcion', Descripcion
+                    )
+            ),''),
+        ']'
+        )AS pSalida 
     FROM Permisos p 
     INNER JOIN PermisosRol pr USING(IdPermiso)
     WHERE pr.IdRol = pIdRol
