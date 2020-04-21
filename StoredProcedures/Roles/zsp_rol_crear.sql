36019b4e4f1dd1d58c523957c67747b2741a1fc5
Loik <loikchoua4@gmail.com>
2020-04-21 19:04:56 -0300

    Cambios en respuesta de SP

--- StoredProcedures/Roles/zsp_rol_crear.sql
+++ StoredProcedures/Roles/zsp_rol_crear.sql
@@ -1,45 +1,72 @@
 DROP PROCEDURE IF EXISTS `zsp_rol_crear`;
 DELIMITER $$
-CREATE PROCEDURE `zsp_rol_crear`(pToken varchar(256), pRol varchar(40), pDescripcion varchar(255))
+CREATE PROCEDURE `zsp_rol_crear`(pIn JSON)
 
 SALIR: BEGIN
 	/*
 		Permite crear un rol controlando que el nombre no exista ya. 
 		Devuelve 'OK'+Id o el mensaje de error en Mensaje.
 	*/
+	DECLARE pRoles JSON;
+	DECLARE pUsuarioEjecuta JSON;
     DECLARE pIdUsuarioEjecuta smallint;
 	DECLARE pMensaje text;
-	DECLARE pIdRol smallint;
-    
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	DECLARE pIdRol tinyint;
+	DECLARE pToken varchar(256);
+	DECLARE pRol varchar(40);
+	DECLARE pDescripcion varchar(255);
+	DECLARE pRespuesta JSON;
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
-		SELECT 'ERR_TRANSACCION' Mensaje;
+		SELECT f_generarRespuesta("ERROR_TRANSACCION", NULL) pOut;
         ROLLBACK;
 	END;
+
+	SET pRoles = pIn ->> "$.Roles";
+	SET pUsuarioEjecuta = pIn ->> "$.UsuariosEjecuta";
+	SET pToken = pUsuarioEjecuta ->> "$.Token";
+	SET pRol = pRoles ->> "$.Rol";
+	SET pDescripcion = pRoles ->> "$.Descripcion";
+
+
+	BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
+		SELECT f_generarRespuesta(CONCAT("ERROR ", COALESCE(@errno, ''), " (", COALESCE(@sqlstate, ''), "): ", COALESCE(@text, '')), NULL) pOut;
+	END;
+
     
 	CALL zsp_usuario_tiene_permiso(pToken, 'zsp_rol_crear', pIdUsuarioEjecuta, pMensaje);
 	IF pMensaje!='OK' THEN
-		SELECT pMensaje Mensaje;
+		SELECT f_generarRespuesta(pMensaje, NULL) pOut;
 		LEAVE SALIR;
 	END IF;
     
 	IF (pRol IS NULL OR pRol = '') THEN
-        SELECT 'ERR_INGRESAR_NOMBREROL' Mensaje;
+		SELECT f_generarRespuesta('ERROR_INGRESAR_NOMBREROL', NULL) pOut;
         LEAVE SALIR;
 	END IF;
     
     IF EXISTS(SELECT Rol FROM Roles WHERE Rol = pRol) THEN
-		SELECT 'ERR_EXISTE_NOMBREROL' Mensaje;
+		SELECT f_generarRespuesta('ERROR_EXISTE_NOMBREROL', NULL) pOut;
 		LEAVE SALIR;
 	END IF;	
 
     START TRANSACTION;
 		
-        INSERT INTO Roles VALUES (DEFAULT, pRol, 'A', NULLIF(pDescripcion,''));
+        INSERT INTO Roles VALUES (DEFAULT, pRol, NOW(), NULLIF(pDescripcion,''));
 		SET pIdRol = (SELECT IdRol FROM Roles WHERE Rol = pRol);
-		SELECT CONCAT('OK',pIdRol) Mensaje;
-
+		SET pRespuesta = (SELECT (CAST(
+			CONCAT(COALESCE(
+					JSON_OBJECT(
+						'IdRol', IdRol, 
+						'Rol', Rol,
+						'FechaAlta', FechaAlta,
+						'Descripcion', Descripcion
+						)
+					,'')
+			)AS JSON)) FROM Roles WHERE Rol = pRol);
+		SELECT f_generarRespuesta(NULL, JSON_OBJECT("Roles", pRespuesta)) AS pOut;
 	COMMIT;
 END $$
 DELIMITER ;
-
--- StoredProcedures/Roles/zsp_rol_dame.sql
+++ StoredProcedures/Roles/zsp_rol_dame.sql
@@ -1,14 +1,38 @@
 DROP PROCEDURE IF EXISTS `zsp_rol_dame`;
 DELIMITER $$
-CREATE PROCEDURE `zsp_rol_dame`(pIdRol tinyint)
+CREATE PROCEDURE `zsp_rol_dame`(pIn JSON)
 
-BEGIN
+SALIR: BEGIN
     /*
         Procedimiento que sirve para instanciar un rol desde la base de datos.
     */
-	SELECT	*
+    DECLARE pRoles JSON;
+    DECLARE pIdRol int;
+    DECLARE pRespuesta JSON;
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		SELECT f_generarRespuesta("ERROR_TRANSACCION", NULL) pOut;
+        ROLLBACK;
+	END;
+
+    SET pRoles = pIn ->> '$.Roles';
+    SET pIdRol = pRoles ->> '$.IdRol';
+
+	SET pRespuesta = (SELECT 	CAST(CONCAT(
+				COALESCE(
+					JSON_OBJECT(
+						'IdRol', IdRol, 
+						'Rol', Rol,
+						'FechaAlta', FechaAlta,
+						'Descripcion', Descripcion
+					)
+				,'')
+			)AS JSON) AS pOut
     FROM	Roles
-    WHERE	IdRol = pIdRol;
+    WHERE	IdRol = pIdRol);
+    SELECT f_generarRespuesta(NULL, JSON_OBJECT("Roles", pRespuesta)) pOut;
+
 END $$
 DELIMITER ;
 
