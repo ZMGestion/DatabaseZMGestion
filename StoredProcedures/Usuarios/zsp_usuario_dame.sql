70dc6fc356401087aa931c43a34a375540c86f07
Nicolas Bachs <nicolas.bachs@gmail.com>
2020-05-12 15:19:01 -0300

    Cambios StoredProcedures Usuarios

--- StoredProcedures/Usuarios/zsp_usuario_dame.sql
+++ StoredProcedures/Usuarios/zsp_usuario_dame.sql
@@ -1,14 +1,60 @@
 DROP PROCEDURE IF EXISTS `zsp_usuario_dame`;
 DELIMITER $$
-CREATE PROCEDURE `zsp_usuario_dame`(pIdUsuario smallint)
+CREATE PROCEDURE `zsp_usuario_dame`(pIn JSON)
 
-BEGIN
+SALIR: BEGIN
+    DECLARE pUsuarios, pUsuariosEjecuta JSON;
+    DECLARE pIdUsuario smallint;
+    DECLARE pIdUsuarioEjecuta smallint;
+    DECLARE pMensaje text;
+    DECLARE pRespuesta JSON;
+    DECLARE pToken varchar(256);
     /*
         Procedimiento que sirve para instanciar un usuario por id desde la base de datos.
     */
-	SELECT	*
-    FROM	Usuarios
-    WHERE	IdUsuario = pIdUsuario;
+
+    SET pUsuarios = pIn ->> '$.Usuarios';
+    SET pUsuariosEjecuta = pIn ->> '$.UsuariosEjecuta';
+    SET pToken = pUsuariosEjecuta ->> '$.Token';
+
+    CALL zsp_usuario_tiene_permiso(pToken, 'zsp_usuario_dame', pIdUsuarioEjecuta, pMensaje);
+	IF pMensaje!='OK' THEN
+		SELECT f_generarRespuesta(pMensaje, NULL) pOut;
+		LEAVE SALIR;
+	END IF;
+
+    SET pIdUsuario = pUsuarios ->> '$.IdUsuario';
+
+	SET pRespuesta = (
+        SELECT CAST(
+				COALESCE(
+					JSON_OBJECT(
+						'IdUsuario', IdUsuario,
+                        'IdRol', IdRol,
+                        'IdUbicacion', IdUbicacion,
+                        'IdTipoDocumento', IdTipoDocumento,
+                        'Documento', Documento,
+                        'Nombres', Nombres,
+                        'Apellidos', Apellidos,
+                        'EstadoCivil', EstadoCivil,
+                        'Telefono', Telefono,
+                        'Email', Email,
+                        'CantidadHijos', CantidadHijos,
+                        'Usuario', Usuario,
+                        'FechaUltIntento', FechaUltIntento,
+                        'FechaNacimiento', FechaNacimiento,
+                        'FechaInicio', FechaInicio,
+                        'FechaAlta', FechaAlta,
+                        'FechaBaja', FechaBaja,
+                        'Estado', Estado
+					)
+				,'') AS JSON)
+        FROM	Usuarios
+        WHERE	IdUsuario = pIdUsuario
+    );
+
+    SELECT f_generarRespuesta(NULL, JSON_OBJECT("Usuarios", pRespuesta)) pOut;
+
 END $$
 DELIMITER ;
 
--- StoredProcedures/Usuarios/zsp_usuario_dame_por_token.sql
+++ StoredProcedures/Usuarios/zsp_usuario_dame_por_token.sql
@@ -1,16 +1,51 @@
 DROP PROCEDURE IF EXISTS `zsp_usuario_dame_por_token`;
 
 DELIMITER $$
-CREATE PROCEDURE `zsp_usuario_dame_por_token`(pToken varchar(256))
+CREATE PROCEDURE `zsp_usuario_dame_por_token`(pIn JSON)
 
-BEGIN
+SALIR: BEGIN
 
     /*
         Procedimiento que sirve para instanciar un usuario por token desde la base de datos.
     */	
-	SELECT	*
-    FROM	Usuarios
-    WHERE	Token = pToken;
+
+    DECLARE pUsuariosEjecuta JSON;
+    DECLARE pIdUsuario int;
+    DECLARE pRespuesta JSON;
+    DECLARE pToken varchar(256);
+    SET pUsuariosEjecuta = pIn ->> '$.UsuariosEjecuta';
+    SET pIdUsuario = pUsuariosEjecuta ->> '$.IdUsuario';
+    SET pToken = pUsuariosEjecuta ->> '$.Token';
+    
+    SET pRespuesta = (
+        SELECT CAST(
+				COALESCE(
+					JSON_OBJECT(
+						'IdUsuario', IdUsuario,
+                        'IdRol', IdRol,
+                        'IdUbicacion', IdUbicacion,
+                        'IdTipoDocumento', IdTipoDocumento,
+                        'Documento', Documento,
+                        'Nombres', Nombres,
+                        'Apellidos', Apellidos,
+                        'EstadoCivil', EstadoCivil,
+                        'Telefono', Telefono,
+                        'Email', Email,
+                        'CantidadHijos', CantidadHijos,
+                        'Usuario', Usuario,
+                        'FechaUltIntento', FechaUltIntento,
+                        'FechaNacimiento', FechaNacimiento,
+                        'FechaInicio', FechaInicio,
+                        'FechaAlta', FechaAlta,
+                        'FechaBaja', FechaBaja,
+                        'Estado', Estado
+					)
+				,'') AS JSON)
+        FROM	Usuarios
+        WHERE	Token = pToken
+    );
+
+    SELECT f_generarRespuesta(NULL, JSON_OBJECT("Usuarios", pRespuesta)) pOut;
 END $$
 DELIMITER ;
 
