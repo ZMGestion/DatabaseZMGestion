35cb4a562f105eec8ea981f22d058bd3708e63bc
Loïk Choua <loikchoua4@gmail.com>
2020-07-03 20:58:05 -0300

    Nuevos procedimientos Telas

---
+++ StoredProcedures/Telas/zsp_tela_borrar.sql
@@ -0,0 +1,67 @@
+DROP PROCEDURE IF EXISTS `zsp_tela_borrar`;
+
+DELIMITER $$
+CREATE PROCEDURE `zsp_tela_borrar`(pIn JSON)
+SALIR:BEGIN
+    /*
+        Procedimiento que permite borrar una tela. Controla que no este siendo utilizada por un ProductoFinal.
+        Devuelve null en respuesta o el error en error.
+    */
+
+    -- Control de permisos
+    DECLARE pUsuariosEjecuta JSON;
+    DECLARE pIdUsuarioEjecuta smallint;
+    DECLARE pToken varchar(256);
+    DECLARE pMensaje text;
+
+    -- Tela a crear
+    DECLARE pTelas JSON;
+    DECLARE pIdTela smallint;
+
+    -- Para la respuesta
+    DECLARE pRespuesta JSON;
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SHOW ERRORS;
+        SET SQL_SAFE_UPDATES = 1;
+        SELECT f_generarRespuesta("ERROR_TRANSACCION", NULL) pOut;
+        ROLLBACK;
+	END;
+
+    SET pUsuariosEjecuta = pIn ->> "$.UsuariosEjecuta";
+    SET pToken = pUsuariosEjecuta ->> "$.Token";
+
+    CALL zsp_usuario_tiene_permiso(pToken, 'zsp_tela_borrar', pIdUsuarioEjecuta, pMensaje);
+    IF pMensaje != 'OK' THEN
+        SELECT f_generarRespuesta(pMensaje, NULL) pOut;
+        LEAVE SALIR;
+    END IF;
+    -- Extraigo atributos de Tela
+    SET pTelas = pIn ->> "$.Telas";
+    SET pIdTela = pTelas ->> "$.IdTela";
+
+    IF NOT EXISTS (SELECT IdTela FROM Telas WHERE IdTela = pIdTela) THEN
+        SELECT f_generarRespuesta("ERROR_NOEXISTE_TELA", NULL) pOut;
+        LEAVE SALIR;
+    END IF;
+
+    IF EXISTS (SELECT IdProductoFinal FROM ProductosFinales WHERE IdTela = pIdTela) THEN
+        SELECT f_generarRespuesta("ERROR_EXISTE_PRODUCTOFINAL_TELA", NULL);
+        LEAVE SALIR;
+    END IF;
+
+    START TRANSACTION;
+        -- Para poder borrar en la tabla precios
+        SET SQL_SAFE_UPDATES = 0;
+        DELETE FROM Telas WHERE IdTela = pIdTela;
+        DELETE FROM Precios WHERE Tipo = 'T' AND  IdReferencia = pIdTela;
+
+        SELECT f_generarRespuesta(NULL, NULL) pOut;
+        SET SQL_SAFE_UPDATES = 1;
+    COMMIT;
+
+
+
+END $$
+DELIMITER ;
---
+++ StoredProcedures/Telas/zsp_tela_dame.sql
@@ -0,0 +1,65 @@
+DROP PROCEDURE IF EXISTS `zsp_tela_dame`;
+
+DELIMITER $$
+CREATE PROCEDURE `zsp_tela_dame` (pIn JSON)
+SALIR:BEGIN
+    /*
+        Procedimiento que devuelve una tela y su precio a partir del IdTela.
+        Devuelve la Tela y el ultimo precio en respuesta o error en error.
+    */
+
+    -- Tela
+    DECLARE pTelas JSON;
+    DECLARE pIdTela smallint;
+
+    -- Precio
+    DECLARE pIdPrecio int;
+
+
+    -- Para la respuesta
+    DECLARE pRespuesta JSON;
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SHOW ERRORS;
+        SELECT f_generarRespuesta("ERROR_TRANSACCION", NULL) pOut;
+        ROLLBACK;
+	END;
+
+    SET pTelas = pIn ->> "$.Telas";
+    SET pIdTela = pTelas ->> "$.IdTela";
+
+    IF NOT EXISTS (SELECT IdTela FROM Telas WHERE IdTela = pIdTela) THEN
+        SELECT f_generarRespuesta("ERROR_NOEXISTE_TELA", NULL) pOut;
+        LEAVE SALIR;
+    END IF;
+    
+    SELECT f_dameUltimoPrecio('T', pIdTela) INTO pIdPrecio;
+
+    SET pRespuesta = (
+			SELECT CAST(
+                JSON_OBJECT(
+                    "Telas",  JSON_OBJECT(
+                        'IdTela', t.IdTela,
+                        'Tela', t.Tela,
+                        'FechaAlta', t.FechaAlta,
+                        'FechaBaja', t.FechaBaja,
+                        'Observaciones', t.Observaciones,
+                        'Estado', t.Estado
+                        ),
+                    "Precios", JSON_OBJECT(
+                        'IdPrecio', p.IdPrecio,
+                        'Precio', p.Precio,
+                        'FechaAlta', p.FechaAlta
+                    ) 
+                )
+             AS JSON)
+			FROM	Telas t
+            INNER JOIN Precios p ON (p.Tipo = 'T' AND t.IdTela = p.IdReferencia)
+			WHERE	t.IdTela = pIdTela AND p.IdPrecio = pIdPrecio
+        );
+	
+		SELECT f_generarRespuesta(NULL, pRespuesta) AS pOut;
+
+END $$
+DELIMITER ;
