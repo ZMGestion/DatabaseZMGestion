6b5fdcf954db71d5f500afa16317568d915a0a14
Loïk Choua <loikchoua4@gmail.com>
2020-07-12 18:27:23 -0300

    Nuevos Buscar con Paginacion

--- StoredProcedures/Productos/zsp_productos_buscar.sql
+++ StoredProcedures/Productos/zsp_productos_buscar.sql
@@ -25,6 +25,13 @@ SALIR:BEGIN
     -- Para la respuesta
     DECLARE pRespuesta JSON;
 
+    -- Paginacion
+    DECLARE pPaginaciones JSON;
+    DECLARE pPagina int;
+    DECLARE pLongitudPagina int;
+    DECLARE pCantidadTotal int;
+    DECLARE pOffset int;
+
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
         SHOW ERRORS;
@@ -49,6 +56,11 @@ SALIR:BEGIN
     SET pIdTipoProducto = pProductos ->> "$.IdTipoProducto";
     SET pEstado = pProductos ->> "$.Estado";
 
+    -- Extraigo atributos de la paginacion
+    SET pPaginaciones = pIn ->>"$.Paginaciones";
+    SET pPagina = pPaginaciones ->> "$.Pagina";
+    SET pLongitudPagina = pPaginaciones ->> "$.LongitudPagina";
+
     IF pIdTipoProducto IS NULL OR pIdTipoProducto = '' THEN
 		SET pIdTipoProducto = 'T';
 	END IF;
@@ -61,12 +73,23 @@ SALIR:BEGIN
     SET pIdCategoriaProducto = COALESCE(pIdCategoriaProducto, 0);
     SET pIdGrupoProducto = COALESCE(pIdGrupoProducto, 0);
 
+    IF pPagina IS NULL OR pPagina = 0 THEN
+        SET pPagina = 1;
+    END IF;
+
+    IF pLongitudPagina IS NULL OR pLongitudPagina = 0 THEN
+        SET pLongitudPagina = (SELECT CAST(Valor AS UNSIGNED) FROM Empresa WHERE Parametro = 'LONGITUDPAGINA');
+    END IF;
+
+    SET pOffset = (pPagina - 1) * pLongitudPagina;
+
     DROP TEMPORARY TABLE IF EXISTS tmp_Productos;
     DROP TEMPORARY TABLE IF EXISTS tmp_ultimosPrecios;
     DROP TEMPORARY TABLE IF EXISTS tmp_preciosProductos;
+    DROP TEMPORARY TABLE IF EXISTS tmp_ResultadosFinal;
 
-
-    CREATE TEMPORARY TABLE tmp_Productos 
+    -- Resultset completo
+    CREATE TEMPORARY TABLE tmp_Productos
     AS SELECT *
     FROM Productos 
 	WHERE	
@@ -77,6 +100,15 @@ SALIR:BEGIN
         (IdGrupoProducto = pIdGrupoProducto OR pIdGrupoProducto = 0)
 	ORDER BY Producto;
 
+    SET pCantidadTotal = (SELECT COUNT(*) FROM tmp_Productos);
+
+    -- Resultset paginado
+    CREATE TEMPORARY TABLE tmp_ResultadosFinal AS
+    SELECT * 
+    FROM tmp_Productos
+    LIMIT pOffset, pLongitudPagina;
+
+
     CREATE TEMPORARY TABLE tmp_preciosProductos AS
     SELECT IdReferencia, MAX(IdPrecio) latestId 
     FROM Precios WHERE Tipo = 'P' GROUP BY IdReferencia;
@@ -86,29 +118,38 @@ SALIR:BEGIN
     FROM tmp_preciosProductos tmp
     INNER JOIN Precios pr ON (pr.IdReferencia = tmp.IdReferencia AND pr.IdPrecio = tmp.latestId);
 
-    SET pRespuesta = (SELECT JSON_ARRAYAGG(
-            JSON_OBJECT(
-                "Productos",
+    SET pRespuesta = (SELECT 
+        JSON_OBJECT(
+            "Paginaciones", JSON_OBJECT(
+                "Pagina", pPagina,
+                "LongitudPagina", pLongitudPagina,
+                "CantidadTotal", pCantidadTotal
+            ),
+            "resultado", 
+                JSON_ARRAYAGG(
                     JSON_OBJECT(
-                        'IdProducto', tp.IdProducto,
-                        'IdCategoriaProducto', tp.IdCategoriaProducto,
-                        'IdGrupoProducto', tp.IdGrupoProducto,
-                        'IdTipoProducto', tp.IdTipoProducto,
-                        'Producto', tp.Producto,
-                        'LongitudTela', tp.LongitudTela,
-                        'FechaAlta', tp.FechaAlta,
-                        'FechaBaja', tp.FechaBaja,
-                        'Observaciones', tp.Observaciones,
-                        'Estado', tp.Estado
-                    ),
-                "Precios", 
-                    JSON_OBJECT(
-                        'IdPrecio', tps.IdPrecio,
-                        'Precio', tps.Precio
+                        "Productos",
+                            JSON_OBJECT(
+                                'IdProducto', tp.IdProducto,
+                                'IdCategoriaProducto', tp.IdCategoriaProducto,
+                                'IdGrupoProducto', tp.IdGrupoProducto,
+                                'IdTipoProducto', tp.IdTipoProducto,
+                                'Producto', tp.Producto,
+                                'LongitudTela', tp.LongitudTela,
+                                'FechaAlta', tp.FechaAlta,
+                                'FechaBaja', tp.FechaBaja,
+                                'Observaciones', tp.Observaciones,
+                                'Estado', tp.Estado
+                            ),
+                        "Precios", 
+                            JSON_OBJECT(
+                                'IdPrecio', tps.IdPrecio,
+                                'Precio', tps.Precio
+                            )
                     )
-            )
+                )
         )
-	FROM tmp_Productos tp
+	FROM tmp_ResultadosFinal tp
     INNER JOIN tmp_ultimosPrecios tps ON (tps.Tipo = 'P' AND tp.IdProducto = tps.IdReferencia)
 	);
 
@@ -117,6 +158,7 @@ SALIR:BEGIN
     DROP TEMPORARY TABLE IF EXISTS tmp_Productos;
     DROP TEMPORARY TABLE IF EXISTS tmp_ultimosPrecios;
     DROP TEMPORARY TABLE IF EXISTS tmp_preciosProductos;
+    DROP TEMPORARY TABLE IF EXISTS tmp_ResultadosFinal;
 
     
 
